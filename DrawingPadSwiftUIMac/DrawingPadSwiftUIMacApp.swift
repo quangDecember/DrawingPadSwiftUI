26e4202d6f886f0b361cde9c67bec7c6ba987049
quang.ha <quangDecember@gmail.com>
2021-08-25 13:34:58 -0400

    add Mac target, works the same

---
+++ DrawingPadSwiftUIMac/DrawingPadSwiftUIMacApp.swift
@@ -0,0 +1,18 @@
+//
+//  DrawingPadSwiftUIMacApp.swift
+//  DrawingPadSwiftUIMac
+//
+//  Created by Quang on 8/25/21.
+//  Copyright © 2021 Mitrevski. All rights reserved.
+//
+
+import SwiftUI
+
+@main
+struct DrawingPadSwiftUIMacApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
